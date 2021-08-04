043e2dccb0fd04f43a2712879c9dd85b501f63f9
GW <kowonp@gmail.com>
2021-08-05 04:29:08 +0900

    Add Description

--- src/Components/Description.js
+++ src/Components/Description.js
@@ -22,15 +22,20 @@ const DescContent = styled.div`
 
 const DescTitle = styled.div`
   color: white;
-  flex: 1;
+  flex: 0.8;
+  padding: 0 80px;
 `;
 
 const DescH1 = styled.h1`
-  font-size: 40px;
+  font-size: 46px;
+  margin-bottom: 20px;
+  line-height: 1.3;
+  font-weight: bold;
 `;
 
 const DescP = styled.p`
   font-size: 25px;
+  line-height: 1.6;
 `;
 
 const DescImageContainer = styled.div`
@@ -57,31 +62,31 @@ const Description = () => {
           </DescImageContainer>
         </DescContent>
         <DescContent>
+          <DescImageContainer>
+            <DescImage src={iconTV}></DescImage>
+          </DescImageContainer>
           <DescTitle>
-            <DescH1>TV로 즐기세요.</DescH1>
-            <DescP>스마트 TV, PlayStation, Xbox, Chromecast, Apple TV, 블루레이 플레이어 등 다양한 디바이스에서 시청하세요.</DescP>
+            <DescH1>즐겨 보는 콘텐츠를 저장해 오프라인으로 시청하세요.</DescH1>
+            <DescP>간편하게 저장하고 빈틈없이 즐겨보세요.</DescP>
+          </DescTitle>
+        </DescContent>
+        <DescContent>
+          <DescTitle>
+            <DescH1>다양한 디바이스에서 시청하세요.</DescH1>
+            <DescP>각종 영화와 TV 프로그램을 스마트폰, 태블릿, 노트북, TV에서 무제한으로 스트리밍하세요. 추가 요금이 전혀 없습니다.</DescP>
           </DescTitle>
           <DescImageContainer>
             <DescImage src={iconTV}></DescImage>
           </DescImageContainer>
         </DescContent>
         <DescContent>
-          <DescTitle>
-            <DescH1>TV로 즐기세요.</DescH1>
-            <DescP>스마트 TV, PlayStation, Xbox, Chromecast, Apple TV, 블루레이 플레이어 등 다양한 디바이스에서 시청하세요.</DescP>
-          </DescTitle>
           <DescImageContainer>
             <DescImage src={iconTV}></DescImage>
           </DescImageContainer>
-        </DescContent>
-        <DescContent>
           <DescTitle>
-            <DescH1>TV로 즐기세요.</DescH1>
-            <DescP>스마트 TV, PlayStation, Xbox, Chromecast, Apple TV, 블루레이 플레이어 등 다양한 디바이스에서 시청하세요.</DescP>
+            <DescH1>어린이 전용 프로필을 만들어 보세요.</DescH1>
+            <DescP>자기만의 공간에서 좋아하는 캐릭터와 즐기는 신나는 모험. 자녀에게 이 특별한 경험을 선물하세요. 넷플릭스 회원이라면 무료입니다.</DescP>
           </DescTitle>
-          <DescImageContainer>
-            <DescImage src={iconTV}></DescImage>
-          </DescImageContainer>
         </DescContent>
       </DescContainer>
     </Container>
